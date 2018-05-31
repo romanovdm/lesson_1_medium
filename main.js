e0d4453e19544f3723bd8cbd8a9975bfe8a1346e
RomanovDm <79024081313@yandex.ru>
2018-05-31 11:44:21 +0300

    оптимизация
    убрал лишнюю переменную, упростил логику

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 var num = 33721;
 	multiplier = 1; //переменная для произведения
-	a = 0;//переменная старта отчёта
-	
+
 num = String(num);
 var i = num.length;
 
 
 while (i) {
-  multiplier = multiplier * num[a];
-  a++;
-  i--
+  multiplier = multiplier * num[i-1];
+  i--;
 }
 
  var kvest = Math.pow(multiplier, 3);
